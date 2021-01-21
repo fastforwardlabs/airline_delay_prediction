0a0b3acd6b2895bd0ef732d634abdabb2f6ab610
andrewrreed <areed1242@gmail.com>
2021-01-21 10:48:51 -0500

    update bootstrap.py to run hdfs commands as subprocesses

--- 0_bootstrap.py
+++ 0_bootstrap.py
@@ -15,12 +15,11 @@
 from cmlbootstrap import CMLBootstrap
 import os
 import xml.etree.ElementTree as ET
+import subprocess
 
 # Set the setup variables needed by CMLBootstrap
-HOST = os.getenv("CDSW_API_URL").split(
-    ":")[0] + "://" + os.getenv("CDSW_DOMAIN")
-USERNAME = os.getenv("CDSW_PROJECT_URL").split(
-    "/")[6]  # args.username  # "vdibia"
+HOST = os.getenv("CDSW_API_URL").split(":")[0] + "://" + os.getenv("CDSW_DOMAIN")
+USERNAME = os.getenv("CDSW_PROJECT_URL").split("/")[6]  # args.username  # "vdibia"
 API_KEY = os.getenv("CDSW_API_KEY") 
 PROJECT_NAME = os.getenv("CDSW_PROJECT")  
 
@@ -43,16 +42,58 @@ except:
   storage_environment = cml.create_environment_variable(storage_environment_params)
   os.environ["STORAGE"] = storage
 
-# Upload the data to the cloud storage
-!mkdir data
-!cp all_flight_data.tgz data
+# define a function to run commands on HDFS
+def run_cmd(cmd):
+
+  """
+  Run Linux commands using Python's subprocess module
+  Args:
+      cmd (str) - Linux command to run
+  Returns:
+      output
+      errors
+  """
+  print("Running system command: {0}".format(cmd))
+
+  args_list = cmd.split(" ")
+  proc = subprocess.Popen(
+      args_list, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+  )
+
+  output, errors = proc.communicate()
+  if proc.returncode != 0:
+      raise RuntimeError(
+          "Error running command: {}. Return code: {}, Error: {}".format(args_list, proc.returncode, errors)
+      )
+      
+  return output, errors
+
+# unzip data tar
 !cd data && tar xjvf all_flight_data.tgz
 
-!hdfs dfs -mkdir -p $STORAGE/datalake
-!hdfs dfs -mkdir -p $STORAGE/datalake/data
-!hdfs dfs -mkdir -p $STORAGE/datalake/data/flight_data
-!hdfs dfs -mkdir -p $STORAGE/datalake/data/flight_data/set_1
-!hdfs dfs -mkdir -p $STORAGE/datalake/data/flight_data/set_2
-
-!curl https://cdp-demo-data.s3-us-west-2.amazonaws.com/all_flight_data.zip | zcat | hadoop fs -put - $STORAGE/datalake/data/flight_data/set_1/flight_data_1.csv
-!for i in $(seq 2009 2018); do curl https://cdp-demo-data.s3-us-west-2.amazonaws.com/$i.csv | hadoop fs -put - $STORAGE/datalake/data/flight_data/set_2/$i.csv; done
+# Attempt to upload the data to the cloud storage, if error,
+# set environment variable indicating the use of local storage
+# for project build
+# TO-DO: troubleshoot the execution of curl statement via process below
+try:
+    out, err = run_cmd(
+        f'hdfs dfs -mkdir -p {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}'
+    )
+    out, err = run_cmd(
+        f'hdfs dfs -mkdir -p {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}/set_1'
+    )
+    out, err = run_cmd(
+        f'hdfs dfs -mkdir -p {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}/set_2'
+    )
+    out, err = run_cmd(
+        f'curl https://cdp-demo-data.s3-us-west-2.amazonaws.com/all_flight_data.zip | zcat | hadoop fs -put - {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}/set_1/flight_data_1.csv'
+    )
+    out, err = run_cmd(
+        f'for i in $(seq 2009 2018); do curl https://cdp-demo-data.s3-us-west-2.amazonaws.com/$i.csv | hadoop fs -put - {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}/set_2/$i.csv; done'
+    )
+except RuntimeError as error:
+    cml.create_environment_variable({"STORAGE_MODE": "local"})
+    print(
+        "Could not interact with external data store so local project storage will be used. HDFS DFS command failed with the following error:"
+    )
+    print(error)
