bbb9b4ef40873b48f099c7ddec6f1c6e1ae4e2a0
andrewrreed <areed1242@gmail.com>
2021-01-26 09:34:49 -0500

    auto format 1_data_ingest.py

--- 1_data_ingest.py
+++ 1_data_ingest.py
@@ -73,17 +73,17 @@ from pyspark.sql.functions import *
 #    .config("spark.dynamicAllocation.enabled","true")\
 #    .config("spark.shuffle.service.enabled","true")\
 
-storage = os.environ['STORAGE']
+storage = os.environ["STORAGE"]
 
-spark = SparkSession\
-    .builder\
-    .appName("PythonSQL")\
-    .config("spark.executor.memory","8g")\
-    .config("spark.executor.cores","2")\
-    .config("spark.driver.memory","6g")\
-    .config("spark.executor.instances","4")\
-    .config("spark.yarn.access.hadoopFileSystems",storage)\
+spark = (
+    SparkSession.builder.appName("PythonSQL")
+    .config("spark.executor.memory", "8g")
+    .config("spark.executor.cores", "2")
+    .config("spark.driver.memory", "6g")
+    .config("spark.executor.instances", "4")
+    .config("spark.yarn.access.hadoopFileSystems", storage)
     .getOrCreate()
+)
 
 # **Note:**
 # Our file isn't big, so running it in Spark local mode is fine but you can add the following config
@@ -98,156 +98,218 @@ spark = SparkSession\
 # read *all* the Data if you try infer the schema.
 
 #!hdfs dfs -copyFromLocal /home/cdsw/data/all_flight_data.csv $STORAGE/datalake/data/flights-external/all_flight_data.csv
-#```
-#month,dayofmonth,dayofweek,deptime,crsdeptime,arrtime,crsarrtime,uniquecarrier,flightnum,tailnum,actualelapsedtime,crselapsedtime,airtime,arrdelay,depdelay,origin,dest,distance,taxiin,taxiout,cancelled,cancellationcode,diverted,carrierdelay,weatherdelay,nasdelay,securitydelay,lateaircraftdelay,year
-#12,8,1,1123,1124,1559,1550,NW,921,N240NW,516,506,493,9,-1,MSP,HNL,3972,5,18,0,"",0,0,0,0,0,0,2003
-#12,7,7,1124,1124,1550,1550,NW,921,N229NW,506,506,482,0,0,MSP,HNL,3972,3,21,0,"",0,0,0,0,0,0,2003
-#```
+# ```
+# month,dayofmonth,dayofweek,deptime,crsdeptime,arrtime,crsarrtime,uniquecarrier,flightnum,tailnum,actualelapsedtime,crselapsedtime,airtime,arrdelay,depdelay,origin,dest,distance,taxiin,taxiout,cancelled,cancellationcode,diverted,carrierdelay,weatherdelay,nasdelay,securitydelay,lateaircraftdelay,year
+# 12,8,1,1123,1124,1559,1550,NW,921,N240NW,516,506,493,9,-1,MSP,HNL,3972,5,18,0,"",0,0,0,0,0,0,2003
+# 12,7,7,1124,1124,1550,1550,NW,921,N229NW,506,506,482,0,0,MSP,HNL,3972,3,21,0,"",0,0,0,0,0,0,2003
+# ```
 set_1_schema = StructType(
     [
-      StructField("month", DoubleType(), True),
-      StructField("dayofmonth", DoubleType(), True),
-      StructField("dayofweek", DoubleType(), True),
-      StructField("deptime", DoubleType(), True),
-      StructField("crsdeptime", DoubleType(), True),
-      StructField("arrtime", DoubleType(), True),
-      StructField("crsarrtime", DoubleType(), True),
-      StructField("uniquecarrier", StringType(), True),
-      StructField("flightnum", DoubleType(), True),
-      StructField("tailnum", StringType(), True),
-      StructField("actualelapsedtime", DoubleType(), True),
-      StructField("crselapsedtime", DoubleType(), True),
-      StructField("airtime", DoubleType(), True),
-      StructField("arrdelay", DoubleType(), True),
-      StructField("depdelay", DoubleType(), True),
-      StructField("origin", StringType(), True),
-      StructField("dest", StringType(), True),
-      StructField("distance", DoubleType(), True),
-      StructField("taxiin", DoubleType(), True),
-      StructField("taxiout", DoubleType(), True),
-      StructField("cancelled", DoubleType(), True),
-      StructField("cancellationcode", StringType(), True),
-      StructField("diverted", DoubleType(), True),
-      StructField("carrierdelay", DoubleType(), True),
-      StructField("weatherdelay", DoubleType(), True),
-      StructField("nasdelay", DoubleType(), True),
-      StructField("securitydelay", DoubleType(), True),
-      StructField("lateaircraftdelay", DoubleType(), True),
-      StructField("year", DoubleType(), True)
+        StructField("month", DoubleType(), True),
+        StructField("dayofmonth", DoubleType(), True),
+        StructField("dayofweek", DoubleType(), True),
+        StructField("deptime", DoubleType(), True),
+        StructField("crsdeptime", DoubleType(), True),
+        StructField("arrtime", DoubleType(), True),
+        StructField("crsarrtime", DoubleType(), True),
+        StructField("uniquecarrier", StringType(), True),
+        StructField("flightnum", DoubleType(), True),
+        StructField("tailnum", StringType(), True),
+        StructField("actualelapsedtime", DoubleType(), True),
+        StructField("crselapsedtime", DoubleType(), True),
+        StructField("airtime", DoubleType(), True),
+        StructField("arrdelay", DoubleType(), True),
+        StructField("depdelay", DoubleType(), True),
+        StructField("origin", StringType(), True),
+        StructField("dest", StringType(), True),
+        StructField("distance", DoubleType(), True),
+        StructField("taxiin", DoubleType(), True),
+        StructField("taxiout", DoubleType(), True),
+        StructField("cancelled", DoubleType(), True),
+        StructField("cancellationcode", StringType(), True),
+        StructField("diverted", DoubleType(), True),
+        StructField("carrierdelay", DoubleType(), True),
+        StructField("weatherdelay", DoubleType(), True),
+        StructField("nasdelay", DoubleType(), True),
+        StructField("securitydelay", DoubleType(), True),
+        StructField("lateaircraftdelay", DoubleType(), True),
+        StructField("year", DoubleType(), True),
     ]
 )
 
-# Now we can read in the data from Cloud Storage into Spark...
+# Now we can read in the dataset 1 into Spark
+storage = os.environ["STORAGE"]
+data_location = os.environ["DATA_LOCATION"]
+hive_database = os.environ["HIVE_DATABASE"]
+hive_table = os.environ["HIVE_TABLE"]
+hive_table_fq = hive_database + "." + hive_table
+set_1_name = "flight_data_1.csv"
 
+if os.environ["STORAGE_MODE"] == "external":
+    path_1 = f"{storage}/{data_location}/set_1/{set_1_name}"
+else:
+    path_1 = "/home/cdsw/data/{set_1_name}"
 
-flights_data_1 = spark.read.csv(
-    "{}/datalake/data/flight_data/set_1/".format(
-        storage),
-    header=True,
-    #inferSchema = True,
-    schema=set_1_schema,
-    sep=','
-)
+flights_data_1 = spark.read.csv(path_1, header=True, schema=set_1_schema, sep=",")
+
 
 # ...and inspect the data.
-
 flights_data_1.show()
-
 flights_data_1.printSchema()
 
-#```
-#FL_DATE,OP_CARRIER,OP_CARRIER_FL_NUM,ORIGIN,DEST,CRS_DEP_TIME,DEP_TIME,DEP_DELAY,TAXI_OUT,WHEELS_OFF,WHEELS_ON,TAXI_IN,CRS_ARR_TIME,ARR_TIME,ARR_DELAY,CANCELLED,CANCELLATION_CODE,DIVERTED,CRS_ELAPSED_TIME,ACTUAL_ELAPSED_TIME,AIR_TIME,DISTANCE,CARRIER_DELAY,WEATHER_DELAY,NAS_DELAY,SECURITY_DELAY,LATE_AIRCRAFT_DELAY,Unnamed: 27
-#2009-01-01,XE,1204,DCA,EWR,1100,1058.0,-2.0,18.0,1116.0,1158.0,8.0,1202,1206.0,4.0,0.0,,0.0,62.0,68.0,42.0,199.0,,,,,,
-#2009-01-01,XE,1206,EWR,IAD,1510,1509.0,-1.0,28.0,1537.0,1620.0,4.0,1632,1624.0,-8.0,0.0,,0.0,82.0,75.0,43.0,213.0,,,,,,
-#```
+# ```
+# FL_DATE,OP_CARRIER,OP_CARRIER_FL_NUM,ORIGIN,DEST,CRS_DEP_TIME,DEP_TIME,DEP_DELAY,TAXI_OUT,WHEELS_OFF,WHEELS_ON,TAXI_IN,CRS_ARR_TIME,ARR_TIME,ARR_DELAY,CANCELLED,CANCELLATION_CODE,DIVERTED,CRS_ELAPSED_TIME,ACTUAL_ELAPSED_TIME,AIR_TIME,DISTANCE,CARRIER_DELAY,WEATHER_DELAY,NAS_DELAY,SECURITY_DELAY,LATE_AIRCRAFT_DELAY,Unnamed: 27
+# 2009-01-01,XE,1204,DCA,EWR,1100,1058.0,-2.0,18.0,1116.0,1158.0,8.0,1202,1206.0,4.0,0.0,,0.0,62.0,68.0,42.0,199.0,,,,,,
+# 2009-01-01,XE,1206,EWR,IAD,1510,1509.0,-1.0,28.0,1537.0,1620.0,4.0,1632,1624.0,-8.0,0.0,,0.0,82.0,75.0,43.0,213.0,,,,,,
+# ```
 
-set_2_schema = StructType([StructField("FL_DATE", DateType(), True),
-    StructField("OP_CARRIER", StringType(), True),
-    StructField("OP_CARRIER_FL_NUM", StringType(), True),
-    StructField("ORIGIN", StringType(), True),
-    StructField("DEST", StringType(), True),
-    StructField("CRS_DEP_TIME", StringType(), True),
-    StructField("DEP_TIME", StringType(), True),
-    StructField("DEP_DELAY", DoubleType(), True),
-    StructField("TAXI_OUT", DoubleType(), True),
-    StructField("WHEELS_OFF", StringType(), True),
-    StructField("WHEELS_ON", StringType(), True),
-    StructField("TAXI_IN", DoubleType(), True),
-    StructField("CRS_ARR_TIME", StringType(), True),
-    StructField("ARR_TIME", StringType(), True),
-    StructField("ARR_DELAY", DoubleType(), True),
-    StructField("CANCELLED", DoubleType(), True),
-    StructField("CANCELLATION_CODE", StringType(), True),
-    StructField("DIVERTED", DoubleType(), True),
-    StructField("CRS_ELAPSED_TIME", DoubleType(), True),
-    StructField("ACTUAL_ELAPSED_TIME", DoubleType(), True),
-    StructField("AIR_TIME", DoubleType(), True),
-    StructField("DISTANCE", DoubleType(), True),
-    StructField("CARRIER_DELAY", DoubleType(), True),
-    StructField("WEATHER_DELAY", DoubleType(), True),
-    StructField("NAS_DELAY", DoubleType(), True),
-    StructField("SECURITY_DELAY", DoubleType(), True),
-    StructField("LATE_AIRCRAFT_DELAY", DoubleType(), True)])
+set_2_schema = StructType(
+    [
+        StructField("FL_DATE", DateType(), True),
+        StructField("OP_CARRIER", StringType(), True),
+        StructField("OP_CARRIER_FL_NUM", StringType(), True),
+        StructField("ORIGIN", StringType(), True),
+        StructField("DEST", StringType(), True),
+        StructField("CRS_DEP_TIME", StringType(), True),
+        StructField("DEP_TIME", StringType(), True),
+        StructField("DEP_DELAY", DoubleType(), True),
+        StructField("TAXI_OUT", DoubleType(), True),
+        StructField("WHEELS_OFF", StringType(), True),
+        StructField("WHEELS_ON", StringType(), True),
+        StructField("TAXI_IN", DoubleType(), True),
+        StructField("CRS_ARR_TIME", StringType(), True),
+        StructField("ARR_TIME", StringType(), True),
+        StructField("ARR_DELAY", DoubleType(), True),
+        StructField("CANCELLED", DoubleType(), True),
+        StructField("CANCELLATION_CODE", StringType(), True),
+        StructField("DIVERTED", DoubleType(), True),
+        StructField("CRS_ELAPSED_TIME", DoubleType(), True),
+        StructField("ACTUAL_ELAPSED_TIME", DoubleType(), True),
+        StructField("AIR_TIME", DoubleType(), True),
+        StructField("DISTANCE", DoubleType(), True),
+        StructField("CARRIER_DELAY", DoubleType(), True),
+        StructField("WEATHER_DELAY", DoubleType(), True),
+        StructField("NAS_DELAY", DoubleType(), True),
+        StructField("SECURITY_DELAY", DoubleType(), True),
+        StructField("LATE_AIRCRAFT_DELAY", DoubleType(), True),
+    ]
+)
 
 
 flights_data_2 = spark.read.csv(
-    "{}/datalake/data/flight_data/set_2/".format(
-        storage),
+    "{}/datalake/data/flight_data/set_2/".format(storage),
     schema=set_2_schema,
     header=True,
-    sep=',',
-    nullValue='NA'
+    sep=",",
+    nullValue="NA",
 )
 
 flights_data_2.show()
 
-flights_data_1 = flights_data_1\
-  .withColumn("FL_DATE",
-              to_date(
-                concat_ws('-',col("year"),col("month"),col("dayofmonth")),
-              'yyyy.0-MM.0-dd.0')
-                )
+flights_data_1 = flights_data_1.withColumn(
+    "FL_DATE",
+    to_date(
+        concat_ws("-", col("year"), col("month"), col("dayofmonth")), "yyyy.0-MM.0-dd.0"
+    ),
+)
 
-flights_data_1 = flights_data_1\
-.withColumnRenamed("deptime","DEP_TIME")\
-.withColumnRenamed("crsdeptime","CRS_DEP_TIME")\
-.withColumnRenamed("arrtime","ARR_TIME")\
-.withColumnRenamed("crsarrtime","CRS_ARR_TIME")\
-.withColumnRenamed("uniquecarrier","OP_CARRIER")\
-.withColumnRenamed("flightnum","OP_CARRIER_FL_NUM")\
-.withColumnRenamed("actualelapsedtime","ACTUAL_ELAPSED_TIME")\
-.withColumnRenamed("crselapsedtime","CRS_ELAPSED_TIME")\
-.withColumnRenamed("airtime","AIR_TIME")\
-.withColumnRenamed("arrdelay","ARR_DELAY")\
-.withColumnRenamed("depdelay","DEP_DELAY")\
-.withColumnRenamed("origin","ORIGIN")\
-.withColumnRenamed("dest","DEST")\
-.withColumnRenamed("distance","DISTANCE")\
-.withColumnRenamed("taxiin","TAXI_IN")\
-.withColumnRenamed("taxiout","TAXI_OUT")\
-.withColumnRenamed("cancelled","CANCELLED")\
-.withColumnRenamed("cancellationcode","CANCELLATION_CODE")\
-.withColumnRenamed("diverted","DIVERTED")\
-.withColumnRenamed("carrierdelay","CARRIER_DELAY")\
-.withColumnRenamed("weatherdelay","WEATHER_DELAY")\
-.withColumnRenamed("nasdelay","NAS_DELAY")\
-.withColumnRenamed("securitydelay","SECURITY_DELAY")\
-.withColumnRenamed("lateaircraftdelay","LATE_AIRCRAFT_DELAY")
+flights_data_1 = (
+    flights_data_1.withColumnRenamed("deptime", "DEP_TIME")
+    .withColumnRenamed("crsdeptime", "CRS_DEP_TIME")
+    .withColumnRenamed("arrtime", "ARR_TIME")
+    .withColumnRenamed("crsarrtime", "CRS_ARR_TIME")
+    .withColumnRenamed("uniquecarrier", "OP_CARRIER")
+    .withColumnRenamed("flightnum", "OP_CARRIER_FL_NUM")
+    .withColumnRenamed("actualelapsedtime", "ACTUAL_ELAPSED_TIME")
+    .withColumnRenamed("crselapsedtime", "CRS_ELAPSED_TIME")
+    .withColumnRenamed("airtime", "AIR_TIME")
+    .withColumnRenamed("arrdelay", "ARR_DELAY")
+    .withColumnRenamed("depdelay", "DEP_DELAY")
+    .withColumnRenamed("origin", "ORIGIN")
+    .withColumnRenamed("dest", "DEST")
+    .withColumnRenamed("distance", "DISTANCE")
+    .withColumnRenamed("taxiin", "TAXI_IN")
+    .withColumnRenamed("taxiout", "TAXI_OUT")
+    .withColumnRenamed("cancelled", "CANCELLED")
+    .withColumnRenamed("cancellationcode", "CANCELLATION_CODE")
+    .withColumnRenamed("diverted", "DIVERTED")
+    .withColumnRenamed("carrierdelay", "CARRIER_DELAY")
+    .withColumnRenamed("weatherdelay", "WEATHER_DELAY")
+    .withColumnRenamed("nasdelay", "NAS_DELAY")
+    .withColumnRenamed("securitydelay", "SECURITY_DELAY")
+    .withColumnRenamed("lateaircraftdelay", "LATE_AIRCRAFT_DELAY")
+)
 
 
-flights_data_1 = flights_data_1.select(["FL_DATE","DEP_TIME","CRS_DEP_TIME","ARR_TIME","CRS_ARR_TIME","OP_CARRIER","OP_CARRIER_FL_NUM","ACTUAL_ELAPSED_TIME","CRS_ELAPSED_TIME","AIR_TIME","ARR_DELAY","DEP_DELAY","ORIGIN","DEST","DISTANCE","TAXI_IN","TAXI_OUT","CANCELLED","CANCELLATION_CODE","DIVERTED","CARRIER_DELAY","WEATHER_DELAY","NAS_DELAY","SECURITY_DELAY","LATE_AIRCRAFT_DELAY"])
-flights_data_2 = flights_data_2.select(["FL_DATE","DEP_TIME","CRS_DEP_TIME","ARR_TIME","CRS_ARR_TIME","OP_CARRIER","OP_CARRIER_FL_NUM","ACTUAL_ELAPSED_TIME","CRS_ELAPSED_TIME","AIR_TIME","ARR_DELAY","DEP_DELAY","ORIGIN","DEST","DISTANCE","TAXI_IN","TAXI_OUT","CANCELLED","CANCELLATION_CODE","DIVERTED","CARRIER_DELAY","WEATHER_DELAY","NAS_DELAY","SECURITY_DELAY","LATE_AIRCRAFT_DELAY"])
+flights_data_1 = flights_data_1.select(
+    [
+        "FL_DATE",
+        "DEP_TIME",
+        "CRS_DEP_TIME",
+        "ARR_TIME",
+        "CRS_ARR_TIME",
+        "OP_CARRIER",
+        "OP_CARRIER_FL_NUM",
+        "ACTUAL_ELAPSED_TIME",
+        "CRS_ELAPSED_TIME",
+        "AIR_TIME",
+        "ARR_DELAY",
+        "DEP_DELAY",
+        "ORIGIN",
+        "DEST",
+        "DISTANCE",
+        "TAXI_IN",
+        "TAXI_OUT",
+        "CANCELLED",
+        "CANCELLATION_CODE",
+        "DIVERTED",
+        "CARRIER_DELAY",
+        "WEATHER_DELAY",
+        "NAS_DELAY",
+        "SECURITY_DELAY",
+        "LATE_AIRCRAFT_DELAY",
+    ]
+)
+flights_data_2 = flights_data_2.select(
+    [
+        "FL_DATE",
+        "DEP_TIME",
+        "CRS_DEP_TIME",
+        "ARR_TIME",
+        "CRS_ARR_TIME",
+        "OP_CARRIER",
+        "OP_CARRIER_FL_NUM",
+        "ACTUAL_ELAPSED_TIME",
+        "CRS_ELAPSED_TIME",
+        "AIR_TIME",
+        "ARR_DELAY",
+        "DEP_DELAY",
+        "ORIGIN",
+        "DEST",
+        "DISTANCE",
+        "TAXI_IN",
+        "TAXI_OUT",
+        "CANCELLED",
+        "CANCELLATION_CODE",
+        "DIVERTED",
+        "CARRIER_DELAY",
+        "WEATHER_DELAY",
+        "NAS_DELAY",
+        "SECURITY_DELAY",
+        "LATE_AIRCRAFT_DELAY",
+    ]
+)
 
 flights_data_all = flights_data_1.unionByName(flights_data_2)
 
 # Now we can store the Spark DataFrame as a file in the local CML file system
 # *and* as a table in Hive used by the other parts of the project.
 
-#flights_data.coalesce(1).write.csv(
+# flights_data.coalesce(1).write.csv(
 #    "file:/home/cdsw/raw/telco-data/",
 #    mode='overwrite',
 #    header=True
-#)
+# )
 
 spark.sql("show databases").show()
 
@@ -257,14 +319,13 @@ spark.sql("show tables in default").show()
 # This is here to create the table in Hive used be the other parts of the project, if it
 # does not already exist.
 
-if ('flights_data_all' not in list(spark.sql("show tables in default").toPandas()['tableName'])):
+if "flights_data_all" not in list(
+    spark.sql("show tables in default").toPandas()["tableName"]
+):
     print("creating the flights_data_all database")
-    flights_data_all\
-        .write.format("parquet")\
-        .mode("overwrite")\
-        .saveAsTable(
-            'default.flights_data_all'
-        )
+    flights_data_all.write.format("parquet").mode("overwrite").saveAsTable(
+        "default.flights_data_all"
+    )
 
 # Show the data in the hive table
 spark.sql("select * from default.flights_data_all").show()
