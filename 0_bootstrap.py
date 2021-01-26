3bebb795961c673f6731b8469a7f247d1ba1c54d
andrewrreed <areed1242@gmail.com>
2021-01-26 09:32:22 -0500

    Update 0_bootstrap.py

--- 0_bootstrap.py
+++ 0_bootstrap.py
@@ -68,32 +68,32 @@ def run_cmd(cmd):
       
   return output, errors
 
-# unzip data tar
-!cd data && tar xjvf all_flight_data.tgz
-
-# Attempt to upload the data to the cloud storage, if error,
-# set environment variable indicating the use of local storage
-# for project build
-# TO-DO: troubleshoot the execution of curl statement via process below
-try:
-    out, err = run_cmd(
-        f'hdfs dfs -mkdir -p {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}'
-    )
-    out, err = run_cmd(
-        f'hdfs dfs -mkdir -p {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}/set_1'
-    )
-    out, err = run_cmd(
-        f'hdfs dfs -mkdir -p {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}/set_2'
-    )
-    out, err = run_cmd(
-        f'curl https://cdp-demo-data.s3-us-west-2.amazonaws.com/all_flight_data.zip | zcat | hadoop fs -put - {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}/set_1/flight_data_1.csv'
-    )
-    out, err = run_cmd(
-        f'for i in $(seq 2009 2018); do curl https://cdp-demo-data.s3-us-west-2.amazonaws.com/$i.csv | hadoop fs -put - {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}/set_2/$i.csv; done'
-    )
-except RuntimeError as error:
-    cml.create_environment_variable({"STORAGE_MODE": "local"})
-    print(
-        "Could not interact with external data store so local project storage will be used. HDFS DFS command failed with the following error:"
-    )
-    print(error)
+if os.environ["STORAGE_MODE"] == 'local':
+    !cd data && tar xjvf preprocessed_flight_data.tgz
+else:
+    # Attempt to download the full datasets to cloud storage, if error,
+    # set environment variable indicating the use of local storage for project build
+    # TO-DO: troubleshoot the execution of curl statement via process below
+    try:
+        out, err = run_cmd(
+            f'hdfs dfs -mkdir -p {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}'
+        )
+        out, err = run_cmd(
+            f'hdfs dfs -mkdir -p {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}/set_1'
+        )
+        out, err = run_cmd(
+            f'hdfs dfs -mkdir -p {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}/set_2'
+        )
+        out, err = run_cmd(
+            f'curl https://cdp-demo-data.s3-us-west-2.amazonaws.com/all_flight_data.zip | zcat | hadoop fs -put - {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}/set_1/flight_data_1.csv'
+        )
+        out, err = run_cmd(
+            f'for i in $(seq 2009 2018); do curl https://cdp-demo-data.s3-us-west-2.amazonaws.com/$i.csv | hadoop fs -put - {os.environ["STORAGE"]}/{os.environ["DATA_LOCATION"]}/set_2/$i.csv; done'
+        )
+    except RuntimeError as error:
+        cml.create_environment_variable({"STORAGE_MODE": "local"})
+        !cd data && tar xjvf preprocessed_flight_data.tgz
+        print(
+            "Could not interact with external data store so local project storage will be used. HDFS DFS command failed with the following error:"
+        )
+        print(error)
