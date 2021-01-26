76f1723ce5ea168e705ceb439b501289ffc06712
andrewrreed <areed1242@gmail.com>
2021-01-26 09:33:26 -0500

    autoformat 7_application.py

--- 7_application.py
+++ 7_application.py
@@ -1,34 +1,42 @@
-from flask import Flask,send_from_directory,request
+from flask import Flask, send_from_directory, request
 import logging
 from pandas.io.json import dumps as jsonify
 import os
 import random
-from IPython.display import Javascript,HTML
+from IPython.display import Javascript, HTML
 
 
-# This reduces the the output to the 
-log = logging.getLogger('werkzeug')
+# This reduces the the output to the
+log = logging.getLogger("werkzeug")
 log.setLevel(logging.ERROR)
 
 
-app= Flask(__name__,static_url_path='')
-@app.route('/')
+app = Flask(__name__, static_url_path="")
+
+
+@app.route("/")
 def home():
     return "<script> window.location.href = '/flask_files/index.html'</script>"
 
-@app.route('/getkey')
+
+@app.route("/getkey")
 def get_key():
-    access_key= ""
+    access_key = ""
     if os.environ.get("SHTM_ACCESS_KEY") is not None:
-        access_key = os.environ.get("SHTM_ACCESS_KEY","")
-    return {"api_key":access_key}
+        access_key = os.environ.get("SHTM_ACCESS_KEY", "")
+    return {"api_key": access_key}
 
 
-@app.route('/flask_files/<path:path>')
+@app.route("/flask_files/<path:path>")
 def send_file(path):
-    return send_from_directory('flask_files', path)
-  
-HTML("<a href='https://{}.{}'>Open Table View</a>".format(os.environ['CDSW_ENGINE_ID'],os.environ['CDSW_DOMAIN']))
+    return send_from_directory("flask_files", path)
 
-if __name__=="__main__":
-    app.run(host='127.0.0.1', port=int(os.environ['CDSW_READONLY_PORT']))
+
+HTML(
+    "<a href='https://{}.{}'>Open Table View</a>".format(
+        os.environ["CDSW_ENGINE_ID"], os.environ["CDSW_DOMAIN"]
+    )
+)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=int(os.environ["CDSW_READONLY_PORT"]))
