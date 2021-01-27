24bb64e70f6b12cc821949a99e38fa6a514bde73
andrewrreed <areed1242@gmail.com>
2021-01-27 15:41:35 -0500

    cleanup

--- 5_model_train.py
+++ 5_model_train.py
@@ -1,3 +1,7 @@
+# The following script loads the sampled dataset that was created in 3_data_processing.py (or loads
+# the locally saved version if STORAGE_MODE=local) and trains an XGBoost classification model to predict
+# if a flight will be cancelled based on a selected set of input features
+
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -14,7 +18,6 @@ from sklearn.compose import ColumnTransformer
 cancelled_flights = pd.read_csv("data/preprocessed_flight_data.csv")
 cancelled_flights = cancelled_flights.dropna()
 
-# X = cancelled_flights[['uniquecarrier','flightnum','origin','dest','crsdeptime','crselapsedtime','distance','week','hour']]
 X = cancelled_flights[
     [
         "OP_CARRIER",
@@ -32,22 +35,19 @@ X = cancelled_flights[
 y = cancelled_flights[["CANCELLED"]]
 
 categorical_cols = ["OP_CARRIER", "OP_CARRIER_FL_NUM", "ORIGIN", "DEST"]
-
 ct = ColumnTransformer(
     [("le", OneHotEncoder(), categorical_cols)], remainder="passthrough"
 )
 
 X_trans = ct.fit_transform(X)
-
 X_train, X_test, y_train, y_test = train_test_split(X_trans, y, random_state=42)
 
 xgbclf = xgb.XGBClassifier()
-
 pipe = Pipeline([("scaler", StandardScaler(with_mean=False)), ("xgbclf", xgbclf)])
-
 pipe.fit(X_trans, y)
 
 score = pipe.score(X_trans, y)
 print("train", score)
+
 dump(pipe, "models/pipe.joblib")
 dump(ct, "models/ct.joblib")
