60134b470583dfb1f7bab4af837d9377f90f69ba
andrewrreed <areed1242@gmail.com>
2021-01-29 09:28:34 -0500

    add source headers

--- code/9_model_check_job.py
+++ code/9_model_check_job.py
@@ -1,3 +1,43 @@
+# ###########################################################################
+#
+#  CLOUDERA APPLIED MACHINE LEARNING PROTOTYPE (AMP)
+#  (C) Cloudera, Inc. 2021
+#  All rights reserved.
+#
+#  Applicable Open Source License: Apache 2.0
+#
+#  NOTE: Cloudera open source products are modular software products
+#  made up of hundreds of individual components, each of which was
+#  individually copyrighted.  Each Cloudera open source product is a
+#  collective work under U.S. Copyright Law. Your license to use the
+#  collective work is as provided in your written agreement with
+#  Cloudera.  Used apart from the collective work, this file is
+#  licensed for your use pursuant to the open source license
+#  identified above.
+#
+#  This code is provided to you pursuant a written agreement with
+#  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute
+#  this code. If you do not have a written agreement with Cloudera nor
+#  with an authorized and properly licensed third party, you do not
+#  have any rights to access nor to use this code.
+#
+#  Absent a written agreement with Cloudera, Inc. (“Cloudera”) to the
+#  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
+#  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED
+#  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO
+#  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND
+#  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU,
+#  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS
+#  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE
+#  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
+#  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES
+#  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF
+#  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
+#  DATA.
+#
+# ###########################################################################
+
 import cdsw, time, os
 import pandas as pd
 from sklearn.metrics import classification_report
@@ -6,33 +46,36 @@ from cmlbootstrap import CMLBootstrap
 model_id = "33"
 
 # Get the various Model CRN details
-HOST = os.getenv("CDSW_API_URL").split(
-    ":")[0] + "://" + os.getenv("CDSW_DOMAIN")
-USERNAME = os.getenv("CDSW_PROJECT_URL").split(
-    "/")[6]  # args.username  # "vdibia"
-API_KEY = os.getenv("CDSW_API_KEY") 
-PROJECT_NAME = os.getenv("CDSW_PROJECT")  
+HOST = os.getenv("CDSW_API_URL").split(":")[0] + "://" + os.getenv("CDSW_DOMAIN")
+USERNAME = os.getenv("CDSW_PROJECT_URL").split("/")[6]  # args.username  # "vdibia"
+API_KEY = os.getenv("CDSW_API_KEY")
+PROJECT_NAME = os.getenv("CDSW_PROJECT")
 
 cml = CMLBootstrap(HOST, USERNAME, API_KEY, PROJECT_NAME)
 
-latest_model = cml.get_model({"id": model_id, "latestModelDeployment": True, "latestModelBuild": True})
+latest_model = cml.get_model(
+    {"id": model_id, "latestModelDeployment": True, "latestModelBuild": True}
+)
 
-Model_CRN = latest_model ["crn"]
+Model_CRN = latest_model["crn"]
 Deployment_CRN = latest_model["latestModelDeployment"]["crn"]
 
 # Read in the model metrics dict.
-model_metrics = cdsw.read_metrics(model_crn=Model_CRN,model_deployment_crn=Deployment_CRN)
+model_metrics = cdsw.read_metrics(
+    model_crn=Model_CRN, model_deployment_crn=Deployment_CRN
+)
 
 # This is a handy way to unravel the dict into a big pandas dataframe.
 metrics_df = pd.io.json.json_normalize(model_metrics["metrics"])
 
-latest_aggregate_metric = metrics_df.dropna(subset=["metrics.accuracy"]).sort_values('startTimeStampMs')[-1:]["metrics.accuracy"]
+latest_aggregate_metric = metrics_df.dropna(subset=["metrics.accuracy"]).sort_values(
+    "startTimeStampMs"
+)[-1:]["metrics.accuracy"]
 
 
 if latest_aggregate_metric.to_list()[0] < 0.6:
-  print("model is below threshold, retraining")
-  cml.start_job(68,{})
-  #TODO reploy new model
+    print("model is below threshold, retraining")
+    cml.start_job(68, {})
+    # TODO reploy new model
 else:
-  print("model does not need to be retrained")
-  
+    print("model does not need to be retrained")
